2fe563796c05316d8a9297b5ee4747f4c930a31b
Kumar <khararekumar99@gmail.com>
2022-01-01 21:45:15 +0530

    Updates

---
+++ precache-manifest.701615c6012f1d484fbed000cc92a4c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "eba31b87bf3e728fad9a",
+    "url": "/Food-Recipe/static/css/main.2fc1799f.chunk.css"
+  },
+  {
+    "revision": "eba31b87bf3e728fad9a",
+    "url": "/Food-Recipe/static/js/main.eba31b87.chunk.js"
+  },
+  {
+    "revision": "3e3029ab388b12148705",
+    "url": "/Food-Recipe/static/js/1.3e3029ab.chunk.js"
+  },
+  {
+    "revision": "4a7ffa399fbdeefe5700",
+    "url": "/Food-Recipe/static/js/2.4a7ffa39.chunk.js"
+  },
+  {
+    "revision": "a7c877f9034aa210fcfe",
+    "url": "/Food-Recipe/static/js/runtime~main.a7c877f9.js"
+  },
+  {
+    "revision": "268fcc0bb4a7c2e0d3d63c9cc9d9359f",
+    "url": "/Food-Recipe/static/media/BGCR.268fcc0b.jpg"
+  },
+  {
+    "revision": "9eaa4d192ff2a0fa082cba69e364f866",
+    "url": "/Food-Recipe/index.html"
+  }
+];
