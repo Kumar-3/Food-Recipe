2bd521592fb0bf7cf448c3d7127379b9a99df957
Kumar <khararekumar99@gmail.com>
2022-01-01 21:29:55 +0530

    'my-commit'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Recipe from "./Recipe";
+import Select from "react-select";
 function App() {
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
+  const APP_ID = "4574d7ab";
+  const APP_KEY = "96792a5f0d4daeaab117939b40988ca6";
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const getRecipe = async () => {
+    const response = await axios.get(url);
+    setRecipes(response.data.hits);
+    console.log(response.data.hits);
+  };
+  useEffect(() => {
+    getRecipe();
+  }, [query]);
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+  const updateQuery = (e) => {
+    e.preventDefault();
+    setQuery(search);
+  };
+  const UpdateSelectedOption = (e) => {
+    setQuery(e["value"]);
+  };
+
+  const options = [
+    { value: "Breakfast", label: "Breakfast" },
+    { value: "Lunch", label: "Lunch" },
+    { value: "Dinner", label: "Dinner" },
+    { value: "Snack", label: "Snack" },
+    { value: "Teatime", label: "Teatime" },
+    { value: "Alcohol-cocktail", label: "Alcohol-cocktail" },
+    { value: "Bread", label: "Bread" },
+    { value: "Biscuits and cookies", label: "Biscuits and cookies" },
+    { value: "Drinks", label: "	Drinks" },
+    { value: "	Desserts", label: "	Desserts" },
+    { value: "	Egg", label: "	Egg" },
+    { value: "Main course", label: "Main course" },
+    { value: "		Omelet", label: "		Omelet" },
+    { value: "	Pancake", label: "	Pancake" },
+    { value: "	Preps", label: "	Preps" },
+    { value: "	Preserve", label: "	Preserve" },
+    { value: "	Salad", label: "	Salad" },
+    { value: "		Sandwiches", label: "		Sandwiches" },
+    { value: "	Soup", label: "	Soup" },
+    { value: "		Starter", label: "		Starter" },
+    { value: "	American", label: "	American" },
+    { value: "	Asian", label: "	Asian" },
+    { value: "	British  ", label: "	British" },
+    { value: "	Chinese", label: "	Chinese" },
+    { value: "	French", label: "	French" },
+    { value: "	Indian", label: "	Indian" },
+    { value: "	Italian", label: "	Italian" },
+    { value: "	Japanese", label: "	Japanese" },
+    { value: "	Mexican    ", label: "	Mexican" },
+  ];
+  const customStyles = {
+    control: (_, { selectProps: { width }}) => ({
+      width: width
+    })};
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="container-fluid">
+          <nav className="navbar navbar-expand-lg">
+            <a className="navbar-brand" href="/">
+              <h2>Food Recipe</h2>
+            </a>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  <h5>Home</h5>
+                </a>
+              </li>
+              <a className="nav-link active" aria-current="page" href="/">
+                <h5>Food Categories</h5>
+              </a>
+
+              <Select
+                styles={customStyles}
+                onChange={UpdateSelectedOption}
+                options={options}
+                width='200px'
+              />
+            </ul>
+
+            <div
+              className="justify-content-center d-flex"
+              id="navbarSupportedContent"
+            >
+              <form onSubmit={updateQuery} className="d-flex">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  value={search}
+                  onChange={updateSearch}
+                />
+                <button className="btn btn-outline-success" type="submit">
+                  Search
+                </button>
+              </form>
+            </div>
+          </nav>
+          <div className="container mt-5">
+            <div className="row justify-content-md-center row-cols-3">
+              {recipes.map((recipe) => (
+                <div className="col">
+                  <Recipe
+                    title={recipe.recipe.label}
+                    calories={recipe.recipe.calories}
+                    image={recipe.recipe.image}
+                    ingredients={recipe.recipe.ingredients}
+                    url={recipe.recipe.url}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Link from '@mui/material/Link';
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+const Recipe = ({ title, calories, image, ingredients, url}) => {
+  const [expanded, setExpanded] = useState(false);
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+  
+  return (
+    <div>
+      <Card sx={{ maxWidth: 320 }}>
+        <CardHeader title={title} />
+        <CardMedia component="img" height="194" image={image} alt={title} />
+        <div><b>Calories: </b>{calories}</div>
+        <Link href={url}>Recipe Link</Link>
+        <div>{Recipe}</div>
+
+        <CardActions disableSpacing>
+          <div>Ingredients</div>
+
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            {ingredients.map((ingredient) => (
+              <Typography>
+                <li>{ingredient.text}</li>
+              </Typography>
+            ))}
+          </CardContent>
+        </Collapse>
+      </Card>
+    </div>
+  );
+};
+export default Recipe;
